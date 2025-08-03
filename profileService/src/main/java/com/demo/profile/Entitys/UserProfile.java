7301a4cb8e03098ee8ae86b8521a2ec801b6003a
pranay <pranaypatel019@gmail.com>
2025-08-03 17:59:21 +0530

    Added annotation to Format json data to XML

--- profileService/src/main/java/com/demo/profile/Entitys/UserProfile.java
+++ profileService/src/main/java/com/demo/profile/Entitys/UserProfile.java
@@ -1,20 +1,33 @@
 package com.demo.profile.Entitys;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
 @Entity
 @Table(name = "USER_PROFILE_DETAILS")
 @EntityListeners(AuditingEntityListener.class)
 @Data
+@JacksonXmlRootElement(localName = "userProfile")
 public class UserProfile {
 
     @Id
